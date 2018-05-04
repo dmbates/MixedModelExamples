24168be735c35c9a4a11e55b41ed736d81492548
Douglas Bates <dmbates@gmail.com>
2018-05-04 14:14:58 -0500

    Add utilities

--- src/comparison.jl
+++ src/comparison.jl
@@ -35,3 +35,24 @@ function doexample(ddir, dnm, model, optimizers::Vector{Symbol}=LNopts)
     end
     Results(dnm, model, optimizers, cvgtyp, nfeval, minobj, secs)
 end
+
+function wrtspec(io, data, model)
+    m = LinearMixedModel(@eval(@formula(Y ~ $(Meta.parse(model)))), CSV.read(joinpath("data", data*".csv"), allowmissing=:none))
+    trms = m.trms
+    n, p = size(trms[end-1])
+    q = sum(x -> size(x, 2), trms) - (p + 1)
+    join(io, [n, p, q, length(getθ(m)), length(trms) - 2], ",")
+end
+
+function crtspec(inpt, outpt)
+    ff = readlines(inpt)
+    open(outpt, "w") do io
+        println(io, "id,data,model,n,p,q,ngrp,ntheta")
+        for j in 2:length(ff)
+            print(io, ff[j], ',')
+            id, data, model = split(ff[j], ',')
+            wrtspec(io, data, model)
+            println(io)
+        end
+    end
+end
